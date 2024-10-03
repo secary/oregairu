967b0de82d4b8473251c2df5629d3736e84720cd
Secary M <secary2010@outlook.com>
2024-10-03 15:40:14 +0930

    renamed engine to kcdb

--- yukinoshita/kcdb/kcdb.py
+++ yukinoshita/kcdb/kcdb.py
@@ -4,22 +4,25 @@
 @Description: This is the sql engine to connect to the local wsl server and access to kancolle db.
 '''
 
-from sqlalchemy import create_engine as ce
+from sqlalchemy import create_engine 
 import pandas as pd
 user = 'root'
 password = '20107991'
 host = '172.20.43.197'
 database = 'kancolle'
 
-engine = ce((f'mysql+pymysql://{user}:{password}@{host}/{database}'))
 
+# Set the engine of connection
+def engine():
+    return create_engine((f'mysql+pymysql://{user}:{password}@{host}/{database}'))
 
-def get_kc_logbook():
-
+# Get the data from logbook
+def logbook():
     query = "SELECT * FROM kancolle.`logbook`;"
-    df_kc_rawdata = pd.read_sql(query, con=engine)
-    
+    df_kc_rawdata = pd.read_sql(query, con=engine())
     return df_kc_rawdata
+
+
 
 
 # print(df)
